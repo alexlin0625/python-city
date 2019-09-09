8c091898d80c26e47bb1fd57a5208c8b5b741a73
Alex Jeffrey Lin <ajlin@bu.edu>
2019-09-09 18:22:58 -0400

    Add existing file

--- outdoors/park.py
+++ outdoors/park.py
@@ -1,3 +1,33 @@
-def draw_park():
-    print("park not found")
-    return
+def draw_park(population):
+    if population > 500:
+        length = 90
+        width = 60
+    elif population > 2500:
+        length = 180
+        width = 120
+    else:
+        length = 45
+        width = 30
+
+    compute_area(length, width)
+    draw_length_park(length)
+    draw_width_park(width)
+
+
+def compute_area(length, width):
+    return length*width
+
+
+def draw_length_park(length):
+    for _ in range(length):
+        print(".", end="")
+    print()
+
+
+def draw_width_park(width):
+    for _ in range(width):
+        print("|", end="")
+
+
+draw_park(50)
+
